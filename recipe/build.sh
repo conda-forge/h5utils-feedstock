32fe4e1acc33c49114b0f9e6089a41ae687cdd29
Chris Hogan <c096h800@gmail.com>
2019-04-12 13:56:58 -0500

    Disable optimization

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ if [[ ${mpi} != "nompi" ]]; then
     CXX=${PREFIX}/bin/mpicxx
 fi
 
-./configure --prefix="${PREFIX}"
+# Default optimization (-O2) works fine in the test, but when the package
+# is installed onto Ubuntu 16.04 (GLIBC 2.23) the h5topng program seg faults.
+# Disabling optimization fixes it. Could be a compiler bug, but I wasn't able
+# to reproduce it in a way that didn't involve conda
+./configure CPPFLAGS="${CPPFLAGS} -O0" --prefix="${PREFIX}"
 make
 make install
